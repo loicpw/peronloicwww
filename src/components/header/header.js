d2c5d006f68986bcbe8bd0fb24579e9e62435784
Loïc Peron <peronloic.us@gmail.com>
2019-03-25 11:06:43 -0600

    add a phantom div into the header to create a "margin"

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -13,6 +13,7 @@ import './header.css';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import theme from 'styled-theming';
+import {uid} from 'react-uid';
 
 
 // TODO - themed background
@@ -23,6 +24,59 @@ const backgroundColor = theme('mode', {
 
 
 /* ---------------------------------------------------------------------
+ — "FixedHeader" —
+ 
+ creates a wrapping div element to hold children components on top.
+
+ `FixedHeader` has a fixed position on top of the viewport. It renders a
+ fix banner that occupied the whole width and whose height depends on
+ its children.
+
+ It displays in flex / column mode.
+
+ renders a "div" element.
+
+ example ::
+
+    <body>
+        <header>
+            <FixedHeader>
+                <h1>my title</h1>
+                <p>this will stick on the top</p>
+            </FixedHeader>
+        </header>
+        <nav>...</nav>
+    </body>
+--------------------------------------------------------------------- */
+class _FixedHeader extends Component {
+  render() {
+    return (
+      <div className={this.props.className} id={this.props.id}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+
+// TODO
+const FixedHeader = styled(_FixedHeader)`
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    margin: 0px;
+    padding: 10px;
+    width: 100vw;
+    height: auto;
+    background-color: ${backgroundColor};
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    z-index: 1;
+`;
+
+
+/* ---------------------------------------------------------------------
  — "Header" —
  
  creates a wrapping header element to hold children components on top.
@@ -30,6 +84,10 @@ const backgroundColor = theme('mode', {
  `Header` has a fixed position on top of the viewport. It renders a fix
  banner that occupied the whole width and whose height depends on its
  children.
+
+ it also creates a phantom "div" inside the header that matches the
+ height of the fixed banner so subsequent elements will respect the
+ header.
 
  It displays in flex / column mode.
 
@@ -45,30 +103,35 @@ const backgroundColor = theme('mode', {
         <nav>...</nav>
     </Header>
 --------------------------------------------------------------------- */
-class _Header extends Component {
+class Header extends Component {
+    constructor(props) {
+        super(props)
+        // store height of the fixed banned
+        this.state = {
+            height: 0
+        }
+    }
+
+    // record the height of the fixed banner (FixedHeader component)
+    componentDidMount() {
+        const height = document.getElementById(uid(this)).clientHeight;
+        this.setState({ height });
+    }
+
     render() {
+        // add a phantom div that matches the height of
+        // the FixedHeader component, which  will act as a
+        // margin for elements following the "header"
         return (
             <header className={this.props.className}>
-              {this.props.children}
+              <FixedHeader id={uid(this)}>
+                {this.props.children}
+              </FixedHeader>
+              <div style={{height: `${this.state.height}px`}} />
             </header>
         );
     }
 }
 
-
-// TODO
-const Header = styled(_Header)`
-    position: fixed;
-    top: 0px;
-    left: 0px;
-    margin: 0px;
-    padding: 10px;
-    width: 100vw;
-    height: auto;
-    background-color: ${backgroundColor};
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-`;
 
 export default Header;
